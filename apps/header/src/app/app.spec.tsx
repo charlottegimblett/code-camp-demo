d927871b887033c9f286d5e7ce890d0adb239f10
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2024-03-26 17:46:27 +1300

    Actually remove all the libs

--- apps/header/src/app/app.spec.tsx
+++ apps/header/src/app/app.spec.tsx
@@ -10,6 +10,6 @@ describe('App', () => {
 
   it('should have a greeting as the title', () => {
     const { getByText } = render(<App />);
-    expect(getByText(/Welcome header/gi)).toBeTruthy();
+    expect(getByText(/Hello/gi)).toBeTruthy();
   });
 });
--- libs/products/src/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './lib/product-list/product-list';
-export * from './lib/products';
