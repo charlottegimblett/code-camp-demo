ef76d6bc3dc5ccc32aac1b265be7e296a285d32e
Charlotte Gimblett <charlotte.gimblett@ptml.com>
2024-03-30 22:57:45 +1300

    Add hero app

---
+++ apps/hero/src/app/app.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+  // Your style here
+`;
+
+export function App() {
+  return (
+    <StyledApp>
+      I AM YOUR HERO
+    </StyledApp>
+  );
+}
+
+export default App;
