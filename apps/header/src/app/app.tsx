17fdae8e56279babb04137be53efde17ef5efdd5
Charlotte Gimblett <charlotte.gimblett@ptml.com>
2024-03-30 22:30:31 +1300

    Add header

--- apps/header/src/app/app.tsx
+++ apps/header/src/app/app.tsx
@@ -1,11 +1,57 @@
 import styled from 'styled-components';
+import { Button } from 'antd';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMagnifyingGlass, faCartShopping, faUser } from "@fortawesome/free-solid-svg-icons"
 
-const StyledApp = styled.div`
-  // Your style here
+const HeaderBox = styled.div`
+  display: flex;
+  width: 100% - 3rem;
+  height: 4rem;
+  background-color: #3B5948;
+  padding: 0 1.5rem;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const LogoText = styled.div`
+  color: #fff;
+  font-weight: 700;
+  font-size: 2rem;
+`;
+
+const Search = styled.div`
+  background-color: #fff;
+  height: 2rem;
+  width 14rem;
+  border-radius: 4px;
+  align-items: center;
+  display: flex;
+  justify-content: end;
+  padding: 0 0.5rem;
+`;
+
+const AccountBox = styled.div`
+  display: flex;
+  gap: 1.5rem;
 `;
 
 export function App() {
-  return <StyledApp>Hello from the header app</StyledApp>;
+  return (
+    <HeaderBox>
+      <div>
+        <LogoText>LOGO</LogoText>
+      </div>
+      <div>
+        <Search>
+          <FontAwesomeIcon icon={faMagnifyingGlass} color='#401506'/>
+        </Search>
+      </div>
+      <AccountBox>
+        <FontAwesomeIcon icon={faCartShopping} size='lg' color='#ffffff'/>
+        <FontAwesomeIcon icon={faUser} size='lg' color='#ffffff' />
+      </AccountBox>
+    </HeaderBox>
+  );
 }
 
 export default App;
