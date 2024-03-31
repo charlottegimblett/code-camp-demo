1c7112c860e560e6b455a0d12fd28cdeb0427135
Charlotte Gimblett <charlotte.gimblett@ptml.com>
2024-03-31 21:41:20 +1300

    Add styles

--- apps/trekking/src/app/app.tsx
+++ apps/trekking/src/app/app.tsx
@@ -1,10 +1,15 @@
 import styled from 'styled-components';
 
 const StyledApp = styled.div`
+  display: flex;
   height: 20rem;
-  width: 20rem;
+  width: calc(100% / 3);
   background-color: #732D14;
-
+  justify-content: center;
+  color: #fff;
+  padding-top: 1rem;
+  font-size: 2rem;
+  font-weight: 700;
 `;
 
 export function App() {
