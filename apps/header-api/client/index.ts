898cdcbb1e057ec74b79e4b3fa7b9a41a3204a3c
Malin Malliya Wadu <malin.malliya.wadu@gmail.com>
2024-04-12 10:31:21 +1200

    Renamed bff to header-api

--- apps/header-api/client/index.ts
+++ apps/header-api/client/index.ts
@@ -1,9 +1,6 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../server';
 
-//     👆 **type-only** import
-// Pass AppRouter as generic here. 👇 This lets the `trpc` object know
-// what procedures are available on the server and their input/output types.
 export const trpc = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
